9c000a1255e66cf38e568c192d4472764ee23ce7
Matheus Amorim <matheus.a.amorim10@gmail.com>
2025-05-21 16:49:54 -0300

    Exemplo atualizado

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose')
 // ip/link - autenticação
 // Obs: Atlas(obter via compass)
 // Para criar um banco de dados personalizado basta escolher um nome no final da String da url (ex: dbassisti)
-const url = ''
+const url = 'mongodb+srv://admin:123senac@atum.dqeot.mongodb.net/dbsistema'
 
 // criar uma variável de apoio para validação
 let conectado = false
